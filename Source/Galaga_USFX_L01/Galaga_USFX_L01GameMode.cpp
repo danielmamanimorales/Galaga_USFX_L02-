4f1934e07319a0c119084b843e21664450b8c680
danielmamanimorales <161166775+danielmamanimorales@users.noreply.github.com>
2024-03-05 18:35:13 -0400

    Initial commit

---
+++ Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.cpp
@@ -0,0 +1,32 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Galaga_USFX_L01GameMode.h"
+#include "Galaga_USFX_L01Pawn.h"
+#include "NaveEnemiga.h"
+
+AGalaga_USFX_L01GameMode::AGalaga_USFX_L01GameMode()
+{
+	// set default pawn class to our character class
+	DefaultPawnClass = AGalaga_USFX_L01Pawn::StaticClass();
+
+	NaveEnemiga01 = nullptr;
+}
+
+void AGalaga_USFX_L01GameMode::BeginPlay()
+{
+	Super::BeginPlay();
+	//Set the game state to playing
+
+	FVector ubicacionNave = FVector(-900.0f, 50.0f, 250.0f);
+	FRotator rotacionNave = FRotator(0.0f, 0.0f, 0.0f);
+
+	UWorld* const World = GetWorld();
+	if (World != nullptr)
+	{
+		// spawn the projectile
+		NaveEnemiga01 = World->SpawnActor<ANaveEnemiga>(ubicacionNave, rotacionNave);
+	}
+
+	NaveEnemiga01->SetPosicion(FVector(2000.0f, 0.0f, 120.0f));
+
+}
---
+++ Source/Galaga_USFX_L01/Galaga_USFX_L01GameMode.h
@@ -0,0 +1,28 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Galaga_USFX_L01GameMode.generated.h"
+
+class ANaveEnemiga;
+
+UCLASS(MinimalAPI)
+class AGalaga_USFX_L01GameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AGalaga_USFX_L01GameMode();
+
+public:
+	ANaveEnemiga* NaveEnemiga01;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+};
+
+
+
